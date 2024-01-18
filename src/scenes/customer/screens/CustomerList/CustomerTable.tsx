33551af11283f57c306271d172a88c5db81d498b
namdao <nam.h.dao@taptap.com.vn>
2024-01-18 23:52:11 +0700

    add filter customer

--- src/scenes/customer/screens/CustomerList/CustomerTable.tsx
+++ src/scenes/customer/screens/CustomerList/CustomerTable.tsx
@@ -1,19 +1,33 @@
-import React, { useEffect, useMemo } from "react";
+import React, { SyntheticEvent, useEffect, useMemo, useState } from "react";
 import Box from "@mui/material/Box";
 import { DataGridPro, GridRow, GridColumnHeaders } from "@mui/x-data-grid-pro";
 import { CustomerColumn } from "scenes/customer/helper/CustomerColumn";
 import { useAppSelector } from "store";
 import { customerSelector } from "scenes/customer/redux/slice";
-import { LinearProgress } from "@mui/material";
+import { Autocomplete, LinearProgress, TextField } from "@mui/material";
 import { useCustomer } from "scenes/customer/hooks/useCustomer";
+import { ISelect } from "scenes/orders/screens/OrderSearch/BlockFilter";
+import { useLocales } from "locales";
 const MemoizedRow = React.memo(GridRow);
 
 const MemoizedColumnHeaders = React.memo(GridColumnHeaders);
 
 const CustomerTable = () => {
+  const { translate } = useLocales();
   const customerList = useAppSelector(customerSelector.getCustomerList);
   const loading = useAppSelector(customerSelector.getLoading);
   const totalRow = useAppSelector(customerSelector.getTotal);
+  const [customer, setCustomer] = useState<ISelect | null>(null);
+  const customerAutoComplete = customerList.map((e) => {
+    return {
+      id: e.id,
+      label: `${e.name} - (Công ty:${e.company?.company_name}) - ${e.phone}`,
+    };
+  });
+
+  const onCustomerChange = (_e: SyntheticEvent<any>, value: ISelect | null) => {
+    setCustomer(value || null);
+  };
   const { getCustomerList } = useCustomer();
   useEffect(() => {
     getCustomerList(customerList.length > 0);
@@ -33,10 +47,28 @@ const CustomerTable = () => {
     );
   }
 
+  const customerFilter = customer
+    ? customerList.filter((e) => e.id === customer.id)
+    : customerList;
+
   return (
     <Box sx={{ height: "100vh", width: "100%" }}>
+      <Autocomplete
+        disablePortal
+        autoHighlight
+        onChange={onCustomerChange}
+        value={customer}
+        options={customerAutoComplete}
+        sx={{ width: "30%" }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label={translate("orders.orderNeedCollect.filterCustomer")}
+          />
+        )}
+      />
       <DataGridPro
-        rows={customerList}
+        rows={customerFilter}
         rowCount={totalRow}
         columns={CustomerColumn}
         disableRowSelectionOnClick
